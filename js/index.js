a8660d252ce85ff3e3a4f511d1f7ffb47188d51a
Ranacode <romerodeveloper@techie.com>
2018-09-26 19:12:57 +0100

    Users table layout finished

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  const usersDataURL = `${window.location.origin}/data/users.json`;
+
+  $("#users-table").DataTable({
+    responsive: true,
+    stateSave: true,
+    paging: false,
+    searching: false,
+    deferRender: true,
+    scrollY: 1000,
+    scroller: {
+      loadingIndicator: true
+    },
+    scrollCollapse: true,
+    ajax: {
+      url: usersDataURL,
+      type: "GET",
+      dataType: "json",
+      dataSrc: ""
+    },
+    columns: [
+      {
+        data: "thumbnail",
+        render: function(data, type, row, meta) {
+          return `<img src="${data}" alt="${data}" />`;
+        }
+      },
+      { data: "name" },
+      { data: "username" },
+      { data: "email" },
+      { data: "phone" }
+    ]
+  });
+});
