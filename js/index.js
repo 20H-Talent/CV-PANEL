7175f12759ba007b4ee7b22d96778f08a7fa96b4
Ranacode <romerodeveloper@techie.com>
2018-09-26 22:36:48 +0100

    Users table with bootstrap4 and created singleton class to handle his behavior

--- js/index.js
+++ js/index.js
@@ -1,6 +1,56 @@
-$(document).ready(function() {
-  const usersDataURL = `${window.location.origin}/data/users.json`;
+const UsersTable = (function() {
+  let instance;
 
+  function init() {
+    getUserData(initTable);
+
+    function getUserData(callback) {
+      const usersDataURL = `${window.location.origin}/data/users.json`;
+      if (!localStorage.getItem("users-list")) {
+        $.getJSON(usersDataURL, function(usersData) {
+          localStorage.setItem("users-list", JSON.stringify(usersData));
+          callback();
+        }).fail(function(err) {
+          console.log(err);
+        });
+      } else {
+        callback();
+      }
+    }
+
+    function initTable() {
+      const usersData = JSON.parse(localStorage.getItem("users-list"));
+      const tableBody = $("table#users-table").find("tbody");
+      usersData.forEach(user => {
+        const { id, name, username, email, phone } = user;
+        tableBody.append(`
+        <tr scope="row">
+          <td>${id}</td>
+          <td>${name}</td>
+          <td>${username}</td>
+          <td>${email}</td>
+          <td>${phone}</td>
+         </tr>`);
+      });
+    }
+
+    return {
+      initTable
+    };
+  }
+
+  return {
+    getInstance: function() {
+      if (!instance) {
+        instance = init();
+      }
+      return instance;
+    }
+  };
+})();
+
+const usersTable = UsersTable.getInstance();
+/*
   $("#users-table").DataTable({
     responsive: true,
     stateSave: true,
@@ -31,4 +81,4 @@ $(document).ready(function() {
       { data: "phone" }
     ]
   });
-});
+  */
