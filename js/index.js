9f23b321508151f9ec1ee4fa140100f6d1f3babc
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2018-09-26 20:09:50 +0100

    fix bug js effect side bar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,44 @@
+$(document).ready(function() {
+  const usersDataURL = `${window.location.origin}/data/users.json`;
+
+  $("#users-table").DataTable({
+    responsive: true,
+    stateSave: true,
+    paging: false,
+    searching: false,
+    deferRender: true,
+    scrollY: 1000,
+    scroller: {
+      loadingIndicator: true
+    },
+    scrollCollapse: true,
+    ajax: {
+      url: usersDataURL,
+      type: "GET",
+      dataType: "json",
+      dataSrc: ""
+    },
+    columns: [
+      {
+        data: "thumbnail",
+        render: function(data, type, row, meta) {
+          return `<img src="${data}" alt="${data}" />`;
+        }
+      },
+      { data: "name" },
+      { data: "username" },
+      { data: "email" },
+      { data: "phone" }
+    ]
+  });
+});
+
+
+
 function openNav() {
-    document.getElementById("mySidenavLeft").style.width = "250px";
+  document.getElementById("mySidenavLeft").style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("mySidenavLeft").style.width = "0";
+  document.getElementById("mySidenavLeft").style.width = "0";
 }
