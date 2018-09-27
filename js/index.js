75f397c67d3641fbf439b5f9907d5951e8cafeb7
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2018-09-27 16:39:27 +0100

    Add left sidebar and right sidebar

--- js/index.js
+++ js/index.js
@@ -1,44 +1,65 @@
-$(document).ready(function() {
-  const usersDataURL = `${window.location.origin}/data/users.json`;
+// $(document).ready(function() {
+//   const usersDataURL = `${window.location.origin}/data/users.json`;
 
-  $("#users-table").DataTable({
-    responsive: true,
-    stateSave: true,
-    paging: false,
-    searching: false,
-    deferRender: true,
-    scrollY: 1000,
-    scroller: {
-      loadingIndicator: true
-    },
-    scrollCollapse: true,
-    ajax: {
-      url: usersDataURL,
-      type: "GET",
-      dataType: "json",
-      dataSrc: ""
-    },
-    columns: [
-      {
-        data: "thumbnail",
-        render: function(data, type, row, meta) {
-          return `<img src="${data}" alt="${data}" />`;
-        }
-      },
-      { data: "name" },
-      { data: "username" },
-      { data: "email" },
-      { data: "phone" }
-    ]
-  });
-});
+//   $("#users-table").DataTable({
+//     responsive: true,
+//     stateSave: true,
+//     paging: false,
+//     searching: false,
+//     deferRender: true,
+//     scrollY: 1000,
+//     scroller: {
+//       loadingIndicator: true
+//     },
+//     scrollCollapse: true,
+//     ajax: {
+//       url: usersDataURL,
+//       type: "GET",
+//       dataType: "json",
+//       dataSrc: ""
+//     },
+//     columns: [
+//       {
+//         data: "thumbnail",
+//         render: function(data, type, row, meta) {
+//           return `<img src="${data}" alt="${data}" />`;
+//         }
+//       },
+//       { data: "name" },
+//       { data: "username" },
+//       { data: "email" },
+//       { data: "phone" }
+//     ]
+//   });
+// });
 
 
+/*******************************
+ *  LEFT SIDE BAR 
+ *******************************/
 
-function openNav() {
+function openNavLeft() {
   document.getElementById("mySidenavLeft").style.width = "250px";
+  document.getElementById("main-left").style.marginLeft = "250px";
 }
 
-function closeNav() {
-  document.getElementById("mySidenavLeft").style.width = "0";
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNavLeft() {
+  document.getElementById("mySidenavLeft").style.width = "40px";
+  document.getElementById("main-left").style.marginLeft = "40px";
+}
+
+/*******************************
+ *  RIGHT SIDE BAR 
+ *******************************/
+
+function openNavRight() {
+  document.getElementById("mySidenavRight").style.width = "250px";
+  document.getElementById("main-right").style.marginRight = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNavRight() {
+  document.getElementById("mySidenavRight").style.width = "40px";
+  document.getElementById("main-right").style.marginRigth = "40px";
 }
