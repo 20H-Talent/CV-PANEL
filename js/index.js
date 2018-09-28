62dfb139f62146924ff5cdab1822b7a6f270c31b
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2018-09-28 10:57:54 +0100

    add fluid structure and effects

--- js/index.js
+++ js/index.js
@@ -1,37 +1,37 @@
-// $(document).ready(function() {
-//   const usersDataURL = `${window.location.origin}/data/users.json`;
+$(document).ready(function() {
+  const usersDataURL = `${window.location.origin}/data/users.json`;
 
-//   $("#users-table").DataTable({
-//     responsive: true,
-//     stateSave: true,
-//     paging: false,
-//     searching: false,
-//     deferRender: true,
-//     scrollY: 1000,
-//     scroller: {
-//       loadingIndicator: true
-//     },
-//     scrollCollapse: true,
-//     ajax: {
-//       url: usersDataURL,
-//       type: "GET",
-//       dataType: "json",
-//       dataSrc: ""
-//     },
-//     columns: [
-//       {
-//         data: "thumbnail",
-//         render: function(data, type, row, meta) {
-//           return `<img src="${data}" alt="${data}" />`;
-//         }
-//       },
-//       { data: "name" },
-//       { data: "username" },
-//       { data: "email" },
-//       { data: "phone" }
-//     ]
-//   });
-// });
+  $("#users-table").DataTable({
+    responsive: true,
+    stateSave: true,
+    paging: false,
+    searching: false,
+    deferRender: true,
+    scrollY: 1000,
+    scroller: {
+      loadingIndicator: true
+    },
+    scrollCollapse: true,
+    ajax: {
+      url: usersDataURL,
+      type: "GET",
+      dataType: "json",
+      dataSrc: ""
+    },
+    columns: [
+      {
+        data: "thumbnail",
+        render: function(data, type, row, meta) {
+          return `<img src="${data}" alt="${data}" />`;
+        }
+      },
+      { data: "name" },
+      { data: "username" },
+      { data: "email" },
+      { data: "phone" }
+    ]
+  });
+});
 
 
 /*******************************
@@ -43,7 +43,7 @@ function openNavLeft() {
   document.getElementById("main-left").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+/* Set the width of the side navigation to 40px and the left margin of the page content to 40px */
 function closeNavLeft() {
   document.getElementById("mySidenavLeft").style.width = "40px";
   document.getElementById("main-left").style.marginLeft = "40px";
@@ -58,8 +58,8 @@ function openNavRight() {
   document.getElementById("main-right").style.marginRight = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+/* Set the width of the side navigation to 40px and the right margin of the page content to 40px */
 function closeNavRight() {
   document.getElementById("mySidenavRight").style.width = "40px";
-  document.getElementById("main-right").style.marginRigth = "40px";
+  document.getElementById("main-right").style.marginRight = "40px";
 }
