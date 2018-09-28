664db15c919ce3c1982d70be36398314b6546752
Yuneysa S <ysuarez@20h.es>
2018-09-28 02:13:13 +0100

    Added styles for right menu and applied js on it

--- js/index.js
+++ js/index.js
@@ -53,3 +53,15 @@ let age = document.querySelector('#age');
 range.addEventListener('click', function(event){
     age.innerHTML = range.value;
 });
+
+let sidebarItems2 = document.querySelectorAll('#right-menu li');
+for(sidebarItem of sidebarItems2){
+    sidebarItem.addEventListener("click", function(){
+        // console.log(event.target);
+        let current = document.getElementsByClassName("sidebar-position");
+        // event.target.classList.remove('active');
+        // event.target.classList.add('sidebar-onclick');
+        current[0].className = current[0].className.replace("sidebar-position", "");
+        this.className += " sidebar-position";        
+    });
+};
