bbee12897d57b41d6b66435be06d9426f33296fe
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-09-29 16:20:46 +0100

    form elimanated,form included in index.html

--- js/form.js
+++ js/form.js
@@ -49,3 +49,14 @@ function reset() {
   var select = document.getElementById("selLanguage");
   select.reset();
 }
+function switchVisible() {
+  if (document.getElementById("Div1")) {
+    if (document.getElementById("Div1").style.display == "none") {
+      document.getElementById("Div1").style.display = "block";
+      document.getElementById("Div2").style.display = "none";
+    } else {
+      document.getElementById("Div1").style.display = "none";
+      document.getElementById("Div2").style.display = "block";
+    }
+  }
+}
