6d0a1584efe990ffb2f174b9971e59f0eaf9a17b
amsauciuc <sauciuc.anamaria.dd@gmail.com>
2018-10-04 17:07:57 +0100

    input selected

--- js/form.js
+++ js/form.js
@@ -11,7 +11,9 @@ function formErrors() {
   document.getElementById("renderize").style.opacity = "100";
   //getting the form by id
   var form = document.getElementById("alertform");
-  var input = document.querySelectorAll("input[type=text]");
+  var input = document.querySelectorAll(
+    "input[type=text],input[type=email],input[type=number],input[type=zip],input[type=address],input[type=select],input[type=telephone]"
+  );
   //looping trought the elements of the form
   for (i = 0; i < input.length; i++) {
     if (input[i].checkValidity()) {
