2e9ea2a47a89d14cfbd71785c6f9f639632a6a80
Yuneysa S <ysuarez@20h.es>
2018-10-11 02:06:05 +0100

    Added 2 methods for principal functions laguages and skills and implemented on right and create form

--- js/languages-skills-jsons.js
+++ js/languages-skills-jsons.js
@@ -1,58 +1,107 @@
-// ******* declare function to get data from languages json. ***********
-function languages() {
-  $.getJSON("../data/languages.json")
-    .done(function(data) {
-      $.each(data, function(d) {
-        // console.log(`Position ${d} [Label ${data[d].label}]`);
-        // console.log(`Position ${d} [Name ${data[d].name}]`);
-        // console.log(`Position ${d} [Defaul value ${data[d].defaultValue}]`);
-        // console.log("\n");
-        $("#languages").append(
-          `<div class="custom-control custom-checkbox col-5">
-            <input type="checkbox" name="${data[d].name}" id="${data[d].label}" value=${data[d].defaultValue} class="custom-control-input">
-            <label for="${data[d].label}" class="custom-control-label">${data[d].label}</label>
-          </div>`
-        );
-      });
-    })
-    .fail(function(jqXHR) {
-      if (jqXHR.statusText !== "OK") {
-        console.log("[ERROR]: on loading json.");
-      }
-    });
-}
-// ************** declare function to get data from skills json. *************
-function skills() {
-  $.getJSON("../data/skills.json")
-    .done(function(data) {
-      $.each(data, function(d) {
-        let divObjectSkill = `<div class="custom-control custom-checkbox mr-3">
-          <input type="checkbox" id="${data[d].label}" class="custom-control-input" value=${data[d].defaultValue} name="${data[d].name}">
-          <label class="custom-control-label" for="${data[d].label}">${data[d].label}</label>
-        </div>`;
-        if(data[d].tag === 'layout'){ //objects 0-1.
-          $('#skills div[class*="col-12"]').append(divObjectSkill);
-        }
-        else if(data[d].tag === 'languages'){ //objects 2-7.
-          $('#skills div[class*="col-5"]').append(divObjectSkill);
-          
-        }
-        else{ //objects 7-14.
-          $('#skills div[class*="col-6"]').append(divObjectSkill);
-        }
-      });
-    })
-    .fail(function(jqXHR) {
-      if (jqXHR.statusText !== "OK") {
-        console.log("[ERROR]: on loading json.");
-      }
-    });
-}
-
-languages();
-skills();
-
 // crear 2 metodos :
   //  - form create
   //  - form search
 // segun que evento sea llamado se llama a un met o a otro
+$('#mySidenavLeft ul li:nth-of-type(2)').on('click', languages);
+$('#mySidenavRight a').on('click', languages);
+$('#mySidenavLeft ul li:nth-of-type(2)').on('click', skills);
+$('#mySidenavRight a').on('click', skills);
+// ******* declare function to get data from languages json. ***********
+function languages(e) {
+  $.getJSON("../data/languages.json")
+  .done(function(data) {
+    //call createForm or searchForm method
+    
+    // console.log(e.target.value);
+    // console.log(e);
+    if(e.currentTarget.value === 0){
+      createForm(data);
+      // to prevent the duplicity of data, removing the event from the dom.
+      $('#mySidenavLeft ul li:nth-of-type(2)').off('click', languages);
+    }
+    else{
+      searchForm(data);
+      $('#mySidenavRight a').off('click', languages);
+    }
+  })
+  .fail(function(jqXHR) {
+    if (jqXHR.statusText !== "OK") {
+      console.log("[ERROR]: on loading json.");
+    }
+  });
+  // method to create form
+  function createForm(data){
+    $.each(data, function(d) {
+      
+      $("#selLanguage").append(
+        `<option value="${data[d].defaultValue}">${data[d].label}</option>`
+      );
+    });
+  }
+  // method to search 
+  function searchForm(data){
+    $.each(data, function(d) {
+      // console.log(`Position ${d} [Label ${data[d].label}]`);
+      // console.log(`Position ${d} [Name ${data[d].name}]`);
+      // console.log(`Position ${d} [Defaul value ${data[d].defaultValue}]`);
+      // console.log("\n");
+      $("#languages").append(
+        `<div class="custom-control custom-checkbox col-5">
+          <input type="checkbox" name="${data[d].name}" id="${data[d].label}" value=${data[d].defaultValue} class="custom-control-input">
+          <label for="${data[d].label}" class="custom-control-label">${data[d].label}</label>
+        </div>`
+      );
+    });
+  }
+}
+// ************** declare function to get data from skills json. *************
+function skills(e) {
+  $.getJSON("../data/skills.json")
+  .done(function(data) {
+    if(e.currentTarget.value === 0){
+      createForm(data);
+      // to prevent the duplicity of data, removing the event from the dom.
+      $('#mySidenavLeft ul li:nth-of-type(2)').off('click', skills);
+    }
+    else{
+      searchForm(data);
+      $('#mySidenavRight a').off('click', skills);
+    }
+  })
+  .fail(function(jqXHR) {
+    if (jqXHR.statusText !== "OK") {
+      console.log("[ERROR]: on loading json.");
+    }
+  });
+  // method to create form
+  function createForm(data){
+    $.each(data, function(d) {
+      $('#skill').append(
+        `<div class="custom-control custom-checkbox custom-control-inline">
+          <input name="${data[d].name}" type="checkbox" class="custom-control-input" id="${data[d].label}" value=${data[d].defaultValue}>
+          <label class="custom-control-label" for="${data[d].label}">${data[d].label}</label>
+        </div`
+      );
+    });
+  }
+  // method to search 
+  function searchForm(data){
+    $.each(data, function(d) {
+      let divObjectSkill = `<div class="custom-control custom-checkbox mr-3">
+        <input type="checkbox" id="${data[d].label}1" class="custom-control-input" value=${data[d].defaultValue} name="${data[d].name}">
+        <label class="custom-control-label" for="${data[d].label}1">${data[d].label}</label>
+      </div>`;
+      if(data[d].tag === 'layout'){ //objects 0-1.
+        $('#skills div[class*="col-12"]').append(divObjectSkill);
+      }
+      else if(data[d].tag === 'languages'){ //objects 2-7.
+        $('#skills div[class*="col-5"]').append(divObjectSkill);
+        
+      }
+      else{ //objects 7-14.
+        $('#skills div[class*="col-6"]').append(divObjectSkill);
+      }
+    });
+  }
+
+}
