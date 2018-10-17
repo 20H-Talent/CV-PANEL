90d8abd5d095c16a0eeb224530b2d4098ef698ed
Yuneysa S <ysuarez@20h.es>
2018-10-18 00:34:29 +0100

    Added functionallity to exit the tour

--- js/guided_visit.js
+++ js/guided_visit.js
@@ -1,9 +1,10 @@
-// $("#Div1").load("../html/guided_visit.html", function() {
-//   $("#guidedVisit").trigger("focus");
-// });
-// $("#Div1").load("../html/guided_visit.html");
-
 // ------- Load the modal welcome ---------
 $.get("../html/guided_visit.html", function(data) {
   $("body").append(data);
+
+  // ------- Inicialize the events for button Skip and Start ---------
+  $(".btn-secondary").on("click", function() {
+    document.querySelector("#guidedVisit").remove();
+  });
 });
+// $("#guidedVisit .btn-secondary").trigger();
