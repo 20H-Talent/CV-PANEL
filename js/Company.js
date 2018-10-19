048a18a0bbab5e21cfb924349e2142d5e170551b
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-10-19 20:54:17 +0100

    object constructor adedd

---
+++ js/Company.js
@@ -0,0 +1,33 @@
+function Company(
+  name,
+  CIF,
+  email,
+  socialnetworks,
+  logo,
+  descripcion,
+  workersNumber,
+  phone,
+  address,
+  socialnetworks
+) {
+  this.socialnetworks = socialnetworks;
+  this.address = address;
+  this.name = name;
+  this.CIF = CIF;
+  this.email = email;
+  this.socialnetworks = socialnetworks;
+  this.logo = logo;
+  this.descripcion = descripcion;
+  this.workersNumber = workersNumber;
+  this.phone = phone;
+
+  this.showInfo = function() {
+    $("#modal").html(
+      `<span>${this.logo} </span>
+                   <span>${this.email} </span>
+                   <span>${this.phone} </span>
+                   <span>${this.descripcion} </span>
+                  <span>${this.socialnetworks.facebook} </span>`
+    );
+  };
+}
---
+++ js/indexCompanies.js
@@ -0,0 +1,52 @@
+//   function options() {
+//     $.getJSON("../data/companies.json").done(function(data) {
+//       console.log(data);
+//       $.each(data, function(i, item) {
+//         $("#modal").append(
+//           `<span${item.name} </span>
+//           <span${item.email} </span>
+//           <span${item.phone} </span>
+//           <span${item.address.city} </span>
+//           <span${item.descripcion} </span>`
+//         );
+//       });
+//     });
+//   }
+
+//   $(".delete").click(function() {
+//     var id = $(this).data("uid");
+//     $("#del").click(function() {
+//       if (id == 1) {
+//         $("#d1").html("");
+//       } else if (id == 2) {
+//         $("#d2").html("");
+//       }
+//     });
+//   });
+var companies = new Companies();
+
+$.getJSON("../data/companies.json")
+  .done(function(data) {
+    $.each(data, function(i, item) {
+      var comp1 = new Company(
+        data[i].name,
+        data[i].CIF,
+        data[i].email,
+        data[i].socialnetworks,
+        data[i].logo,
+        data[i].descripcion,
+        data[i].workersNumber,
+        data[i].phone,
+        data[i].address,
+        data[i].socialnetworks
+      );
+      companies.addCompany(comp1);
+    });
+    companies.renderTable();
+    // companies.buttonOpciones();
+  })
+  .fail(function(jqXHR) {
+    if (jqXHR.statusText !== "OK") {
+      console.log("[ERROR]: on loading json.");
+    }
+  });
