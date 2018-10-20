a953dc670c24845693793fb7e16685c8fe680d28
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-10-20 23:13:11 +0100

    showInfo function changed in Company.js file,modal colors changed,

--- js/Company.js
+++ js/Company.js
@@ -23,25 +23,5 @@ function Company(
     this.workersNumber = workersNumber;
     this.phone = phone;
 
-    this.showInfo = function() {
-        setTimeout($("#modal").html(
-            `<div class="shadow-lg p-3 bg-dark rounded" >
-            <div class="card-header d-flex bg-info flex-row align-items-center">
-               <img class="img-fluid mr-2" src=${this.logo} width=120px height:60px  alt="test"/>
-              <div class=" text-light">
-              <h4 class="modal-title"> <p>${this.name}</p></h4></div>
-            </div>
-           <div class="text-light">
-           <div class="card-subtitle mt-3">Email:  <p><a href="mailto:${this.email}">${this.email}</a></p> </div>
-           <div class="card-subtitle">Profile:  <p>${this.descripcion}</p> </div>
-           <div class="card-subtitle">Phone:  <p>${this.phone}</p> </div>
-           <div class="card-subtitle">Numbers of workers:  <p>${this.workersNumber}</p> </div>
-           <div class="card-subtitle">Address:  <p>${this.address.country} ~ ${this.address.city} ${this.address.street} / ${this.address.zipcode}</p> </div>
-           <div class="card-footer  bg-info text-right">
-           </div>
-         </div> `
-        ), 6000);
-        console.log('this.id :', this.id);
 
-    };
 }
--- js/Companys.js
+++ js/Companys.js
@@ -4,23 +4,37 @@ function Companies() {
         this.companies.push(company);
     };
     this.renderTable = function() {
-        console.log("entro aqui", this.companies);
+        console.log("companies", this.companies);
         //companies.addCompany(comp1);
         for (var i = 0; i < this.companies.length; i++) {
             console.log("algo", i);
-            $("#tableBody").append(`<tr scope="row"  data-id=${this.companies[i].id}>    <td class="company-logo user-avatar  pl-5"><img class="img text-center align-middle"src=${this.companies[i].logo} style="width:50px;"/></td>
-                 <td class="fullname"><p>${this.companies[i].name}</p></td>
-                      <td class="company-email">${this.companies[i].email}</td>
-                      <td class="company-registered">${this.companies[i].phone}</td>
-                      <td class="company-city">${this.companies[i].address.city}</td>
-                      <td class="company-registered">${this.companies[i].descripcion}</td>
-                      <td class="company-registered">${this.companies[i].socialnetworks.facebook}</td>
-                      <td class="options text-center">
-                      <button  type="button" onclick=" ${this.companies[i].showInfo()}"  data-id=${this.companies[i].id}   class="btn btn-danger" data-toggle="modal" data-target="#ordine"><i class="far fa-eye"></i></button>
-                      <button type="button" rel="tooltip" id="editForm" class="btn btn-outline-success btn-just-icon btn-sm" data-original-title="" title=""><i class="fas fa-user-edit"></i>
-                      </button>
-                      <button type="button" class=" id="deleteCompany" my-2 btn btn-outline-danger btn-sm " data-toggle="modal" data-target="#delete" data-uid="2"><i class="far fa-trash-alt"></i></button></td>
-                 </tr>`);
+            $("#tableBody").append(`<tr scope="row"  data-id=${this.companies[i].id}  >    <td class="company-logo user-avatar  pl-5"><img class="img text-center align-middle"src=${this.companies[i].logo} style="width:50px;"/></td>
+                <td class="fullname"><p>${this.companies[i].name}</p></td>
+                    <td class="company-email">${this.companies[i].email}</td>
+                    <td class="company-registered">${this.companies[i].phone}</td>
+                    <td class="company-city">${this.companies[i].address.city}</td>
+                    <td class="company-registered">${this.companies[i].descripcion}</td>
+                    <td class="company-registered">${this.companies[i].socialnetworks.facebook}</td>
+                    <td class="options text-center">
+                    <button  type="button" onclick="showInfo(${this.companies[i].id})"   class="btn btn-danger" data-toggle="modal" data-target="#ordine"><i class="far fa-eye"></i></button>
+                    <button type="button" rel="tooltip" id="editForm"  class="btn btn-outline-success btn-just-icon btn-sm" data-original-title="" title=""><i class="fas fa-user-edit"></i>
+                    </button>
+                    <button type="button" class=" id="deleteCompany" my-2 btn btn-outline-danger btn-sm "   data-toggle="modal" data-target="#delete" data-uid="2"><i class="far fa-trash-alt"></i></button></td>
+                </tr>`);
         }
     };
+    this.getCompanyByiD = function(id) {
+        var company = this.companies.filter(function(company) {
+            return company.id == id
+        });
+
+        return company[0];
+    }
+
+
+
+
+
+
+
 }
