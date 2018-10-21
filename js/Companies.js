5b260eb15c5bc824bb90b9f39d75644cdd4172f5
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-10-21 23:20:19 +0100

    color buttons changed

--- js/Companies.js
+++ js/Companies.js
@@ -20,20 +20,21 @@ function Companies() {
         <div class="container">
             <div class="row">
                 <div class="col-md-12 text-center">
-                    <a href="${this.companies[i].socialnetworks.linkedin}"title="Linkedin"  target="_blank"><i class="fab  btn-ldeep-purple fa-lg fa-linkedin "></i></a>
+                  
                     <a href="${this.companies[i].socialnetworks.instagram}"title="Instagram"  target="_blank"><i class="fab btn-instagram  fa-lg fa-instagram"></i></a>
                     <a href="${this.companies[i].socialnetworks.twitter}"title="Twitter"  target=" title="Linkedin" _blank"><i class="fab fa-lg fa-twitter"></i></a>
                     <a href="${this.companies[i].socialnetworks.youtube}"title="Youtube" target="_blank"><i class="fab fa-lg fa-youtube text-danger"></i></a>
                     <a href="${this.companies[i].socialnetworks.facebook}"title="Facebook" target="_blank" ><i class="fab fa-lg fa-facebook-square"></i></a>
+                    <a href="${this.companies[i].socialnetworks.linkedin}"title="Linkedin"  target="_blank"><i class="fab  btn-ldeep-purple fa-lg fa-linkedin "></i></a>
                 </div>
             </div>
         </div>
                 </td>
                 <td class="options text-center">
-                    <button  type="button" onclick="showInfo(${this.companies[i].id})" title="View company"   class="btn  btn-sm  btn-ldeep-purple preview-company " data-toggle="modal" data-target="#ordine"><i class="far fa-eye"></i> </button>
-                    <button type="button" rel="tooltip" title="Edit company"    class="btn btn-sm btn-ldeep-purple  edit-company " data-original-title="" title=""><i class="fas fa-user-edit"></i>
+                    <button  type="button" onclick="showInfo(${this.companies[i].id})" title="View company"   class="btn  btn-sm  btn-outline-primary preview-company " data-toggle="modal" data-target="#ordine"><i class="far fa-eye"></i> </button>
+                    <button type="button" rel="tooltip" title="Edit company"    class="btn btn-sm btn-outline-primary  edit-company " data-original-title="" title=""><i class="fas fa-user-edit"></i>
                     </button>
-                    <button type="button" title="Delete company"  class="my-2 btn  remove-company  btn-sm  btn-ldeep-purple " onclick="removeCompanyFromDOM(${this.companies[i].id})" ><i class="far fa-trash-alt"></i></button></td>
+                    <button type="button" title="Delete company"  class="my-2 btn  remove-company  btn-sm  btn-outline-primary " onclick="removeCompanyFromDOM(${this.companies[i].id})" ><i class="far fa-trash-alt"></i></button></td>
                 </tr>`);
         }
     };
