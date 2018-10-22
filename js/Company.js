b46a7f7eaf7c716e98c865df63bb7fe15befca05
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-10-22 13:50:31 +0100

    for in trying in the social networks

--- js/Company.js
+++ js/Company.js
@@ -25,16 +25,25 @@ function Company(
     this.renderSocialNetworks = function() {
         var divCol = document.createElement("div");
         console.log("social", this.socialnetworks);
-        var objectResult = Object.keys(this.socialnetworks)
+        // var objectResult = Object.keys(this.socialnetworks)
         console.log('objectResult :', objectResult);
-        for (let i = 0; i < objectResult.length; i++) {
-            console.log("entro al goro");
-            var network = objectResult[i];
-            console.log("NETWORK", network);
-            var innerNetwork = "<a href='" + network + "'title='" + network + "'><i class='fab ml-3 btn" + network + "  fa-lg fa-" + network + "'></i></a>";
-            console.log("innerNetwork", innerNetwork);
-            divCol.innerHTML = divCol.innerHTML + innerNetwork;
-        };
+        // for (let i = 0; i < objectResult.length; i++) {
+        //     console.log("entro al goro");
+        //     var network = objectResult[i];
+        //     console.log("NETWORK", network);
+        //     var innerNetwork = "<a href='" + network + "'title='" + network + "'><i class='fab ml-3 btn" + network + "  fa-lg fa-" + network + "'></i></a>";
+        //     console.log("innerNetwork", innerNetwork);
+        //     divCol.innerHTML = divCol.innerHTML + innerNetwork;
+        // };
+
+
+        // for (let social in this.socialnetworks) {
+        //     console.log("entro al goro");
+        //     console.log("NETWORK", this.socialnetworks[social]);
+        //     var innerNetwork = "<a href='" + this.socialnetworks[social] + "'title='" + this.socialnetworks[social] + "'><i class='fab ml-3 btn " + this.socialnetworks[social] + "  fa-lg fa-" + this.socialnetworks[social] + "'></i></a>";
+        //     console.log("innerNetwork", innerNetwork);
+        //     divCol.innerHTML = divCol.innerHTML + innerNetwork;
+        // }
         console.log('divCol :', divCol.innerHTML);
         console.log(this.id);
         console.log('divCol.innerHTML :', divCol.innerHTML);
