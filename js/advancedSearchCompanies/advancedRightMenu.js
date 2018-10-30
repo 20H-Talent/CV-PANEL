099ff105c8561dc84fcce59f469e978418a477f7
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-10-30 02:22:50 +0000

    trying to empty the container before adding the new form

--- js/advancedSearchCompanies/advancedRightMenu.js
+++ js/advancedSearchCompanies/advancedRightMenu.js
@@ -1,27 +1,11 @@
-// $(document).ready(function() {
-//     $("#list-companies").click(function() {
-//         //let mainRight = $("#right-menu-ajax").empty();
-//         $("#search-company").load("../html/searchCompanyTable.html", function() {
-//             let mainRight = $("#right-menu-ajax").empty();
-//             //  mainRight = $("#right-menu-ajax").empty().append(this);
-
-//         });
-//     });
-// });
-
-
-
-
+// This function loads with ajax the html section of search advanced company and puts it in the  menu right on click,(replacing)empting the section of users search advanced
 $(document).ready(function() {
-    $("#list-companies").click(function() {
+    $("#open-icon-right").click(function() {
         $.ajax({
             url: "../html/searchCompanyTable.html",
             dataType: "html",
             success: function(data) {
-                console.log('data :', data);
-                let mainRight = $("#right-menu-ajax").empty().append(data);
-
-                console.log('data :', data);
+                $("#right-menu-ajax").empty().append(data);
             }
         });
     });
