ce12082b9f8c7cf110e5d1ade29dec5405a86565
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-11-04 23:29:14 +0000

    succes ajax changed to done

--- js/advancedSearchCompanies/advancedRightMenu.js
+++ js/advancedSearchCompanies/advancedRightMenu.js
@@ -1,12 +1,13 @@
  //function loads with ajax the html section of search advanced company and puts it in the menu right on click, (replacing) empting the section of users search advanced
  $(document).ready(function() {
      $("#list-companies").click(function() {
-         $.ajax({
-             url: "../html/searchCompaniesForm.html",
-             dataType: "html",
-             success: function(data) {
+         $.get("../html/searchCompaniesForm.html")
+             .done(function(data) {
                  $("#right-menu-ajax").empty().append(data);
-             }
-         });
+             }).fail(function(jqXHR) {
+                 if (jqXHR.statusText !== "OK") {
+                     console.log("[ERROR]: on loading json.");
+                 }
+             });;
      });
  });
