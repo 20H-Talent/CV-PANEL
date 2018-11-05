9c6ab82e25a1c00b6ce3f0a2ed16e9e600b86f9e
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-11-05 21:47:21 +0000

    no validate added to form

--- js/advancedSearchCompanies/advancedRightMenu.js
+++ js/advancedSearchCompanies/advancedRightMenu.js
@@ -12,74 +12,74 @@
      });
  });
 
- function createRequestBody(company) {
-     let formData = new FormData();
+ //  function createRequestBody(company) {
+ //      let formData = new FormData();
 
-     // Sent as a string
-     formData.append('name', company.name);
-     // Sent as a string
-     formData.append('CIF', company.CIF);
+ //      // Sent as a string
+ //      formData.append('name', company.name);
+ //      // Sent as a string
+ //      formData.append('CIF', company.CIF);
 
-     // Sent as a string with email validation
-     formData.append('email', company.email);
+ //      // Sent as a string with email validation
+ //      formData.append('email', company.email);
 
-     // Sent as a string
-     //  formData.append('city', company.address.city);
+ //      // Sent as a string
+ //      //  formData.append('city', company.address.city);
 
-     // Sent as a string
-     formData.append('country', company.address.country);
-     formData.append('zipcode', company.address.zipcode);
+ //      // Sent as a string
+ //      formData.append('country', company.address.country);
+ //      formData.append('zipcode', company.address.zipcode);
 
-     // Sent as a string
-     formData.append('street', company.address.street);
+ //      // Sent as a string
+ //      formData.append('street', company.address.street);
 
-     // Sent as a string
-     formData.append('bio', company.bio);
+ //      // Sent as a string
+ //      formData.append('bio', company.bio);
 
-     // Sent as a string
-     formData.append('employees', company.employees);
+ //      // Sent as a string
+ //      formData.append('employees', company.employees);
 
-     // Sent as a string. Choose a value from below.
-     formData.append('phone', company.phone);
+ //      // Sent as a string. Choose a value from below.
+ //      formData.append('phone', company.phone);
 
-     //  formData.append('logo', company.logoURL.files[0]);
+ //      //  formData.append('logo', company.logoURL.files[0]);
 
-     // Pick the value from an input(type="date")
-     console.log('formData :', formData);
-     return formData;
- }
-
-
+ //      // Pick the value from an input(type="date")
+ //      console.log('formData :', formData);
+ //      return formData;
+ //  }
 
 
- function sendNewCompany(e) {
 
-     let company = new Company(
-         $("input[name=name]").val(),
-         $("input[name=CIF]").val(),
-         $("input[name=country]").val(),
-         $("input[name=city]").val(),
-         $("input[name=street]").val(),
-         $("input[name=phone]").val(),
-         $("input[name=email]").val(),
-         $("input[name=employees]").val(),
-         $("input[name=website]").val(),
-         $("input[name=bio]").val(),
-         $("input[name=logoURL]").val()
-     );
-     let formBody = createRequestBody(company);
 
-     fetch("https://cv-mobile-api.herokuapp.com/api/company", {
-             method: 'POST',
-             body: formBody
-         })
-         .then(res => res.json())
-         .then(response => console.log(response));
- }
- $("#alert-form-enterprises").on("submit", function(e) {
-     e.preventDefault();
+ //  function sendNewCompany(e) {
 
-     console.log($(this).serialize());
-     sendNewCompany();
+ //      let company = new Company(
+ //          $("input[name=name]").val(),
+ //          $("input[name=CIF]").val(),
+ //          $("input[name=country]").val(),
+ //          $("input[name=city]").val(),
+ //          $("input[name=street]").val(),
+ //          $("input[name=phone]").val(),
+ //          $("input[name=email]").val(),
+ //          $("input[name=employees]").val(),
+ //          $("input[name=website]").val(),
+ //          $("input[name=bio]").val(),
+ //          $("input[name=logoURL]").val()
+ //      );
+ //      let formBody = createRequestBody(company);
 
- })
+ //      fetch("https://cv-mobile-api.herokuapp.com/api/company", {
+ //              method: 'POST',
+ //              body: formBody
+ //          })
+ //          .then(res => res.json())
+ //          .then(response => console.log(response));
+ //  }
+ //  $("#alert-form-enterprises").on("submit", function(e) {
+ //      e.preventDefault();
+
+ //      console.log($(this).serialize());
+ //      sendNewCompany();
+
+ //  })
