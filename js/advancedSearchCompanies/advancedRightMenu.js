4aa62f92284be821ae6ec31d222acbcdbb69bc15
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-11-05 14:11:45 +0000

    new company to send to api

--- js/advancedSearchCompanies/advancedRightMenu.js
+++ js/advancedSearchCompanies/advancedRightMenu.js
@@ -11,3 +11,56 @@
              });;
      });
  });
+
+ function createRequestBody(company) {
+     let formData = new FormData();
+
+     // Sent as a string
+     formData.append('name', company.name);
+     º
+     // Sent as a string
+     formData.append('CIF', company.CIF);
+
+     // Sent as a string with email validation
+     formData.append('email', company.email);
+
+     // Sent as a string
+     formData.append('city', company.address.city);
+
+     // Sent as a string
+     formData.append('country', company.address.country);
+
+     // Sent as a string
+     formData.append('jobTitle', company.address.street);
+
+     // Sent as a string
+     formData.append('bio', company.bio);
+
+     // Sent as a string
+     formData.append('employees', company.employees);
+
+     // Sent as a string. Choose a value from below.
+     formData.append('phone', company.phone);
+
+     // Pick the value from an input(type="date")
+
+     return formData;
+ }
+
+
+
+
+ function sendNewUser(e) {
+     e.preventDefault();
+     let company = new Company(
+         $("input[name=inputName]").val(), $("input[name=inputCif]").val(), $("input[name=imputAdress]").val(), $("input[name=imputCity]").val(), $("input[name=inputCountry]").val(), $("input[name=inputZip]").val(), $("input[name=inputEmail]").val(), $("input[name=inputWorkersNumber]").val(), $("input[name=inputSocialNetworks]").val(), $("input[name=inputLogo]").val(), $("input[name=inputDescription]").val()
+     );
+     let formBody = createRequestBody(company);
+
+     fetch("https://cv-mobile-api.herokuapp.com/api/company", {
+             method: 'POST',
+             body: formBody
+         })
+         .then(res => res.json())
+         .then(response => console.log(response));
+ }
