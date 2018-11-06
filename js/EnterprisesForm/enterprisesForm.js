2875b9fbe9460d1bc4f6b5314d5cdc8501757e2d
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-11-06 00:17:06 +0000

    new company moved into form enterprise

--- js/EnterprisesForm/enterprisesForm.js
+++ js/EnterprisesForm/enterprisesForm.js
@@ -16,6 +16,77 @@
  */
 
 function verifyFormEnterprises() {
+
+
+    /***********************************
+     * Adding a new company to API
+     ***********************************/
+    let name = $("input[name=name]").val();
+    let CIF = $("input[name=CIF]").val();
+    let country = $("input[name=country]").val();
+    let zipcode = $("input[name=zipcode]").val();
+    let city = $("input[name=city]").val();
+    let street = $("input[name=street]").val();
+    let phone = $("input[name=phone]").val();
+    let email = $("input[name=email]").val();
+    let employees = $("input[name=employees]").val();
+    let website = $("input[name=website]").val();
+    let bio = $("input[name=bio]").val();
+    let logoURL = $("input[name=logoURL]");
+    logoURL = [];
+    let socialUrls = $("input[name=socialUrls]");
+    let fileField = document.querySelector("input[type='file']");
+    console.log('fileField :', logoURL);
+    console.log('name :', name);
+    $("#alert-form-enterprises").on("submit", function() {
+        event.preventDefault();
+        var neewCompany = sendNewCompany();
+        console.log(neewCompany);
+        fetch('https://cv-mobile-api.herokuapp.com/api/company', {
+                method: 'POST',
+                body: neewCompany
+            })
+            .then(res => {
+
+                return res.json()
+            })
+            .then(response => console.log(response));
+
+        //compiErrorData.classList.add("d-none");
+
+    });
+
+    function sendNewCompany() {
+
+        let formData = new FormData();
+        formData.append('name', name);
+        formData.append('CIF', CIF);
+        formData.append('email', email);
+        formData.append('country', country);
+        formData.append('zipcode', zipcode);
+        formData.append('street', street);
+        formData.append('bio', bio);
+        formData.append('employees', employees);
+        formData.append('phone', phone);
+        formData.append('city', city);
+        formData.append('logoURL', logoURL[0]);
+        formData.append('website', website);
+        formData.append('socialUrls', socialUrls);
+        console.log('formData :', formData);
+        return formData;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     var form = document.getElementById("alert-form-enterprises");
     var inputsForm = form.querySelectorAll(
         "input[type=text],input[type=email],input[type=tel]"
@@ -61,100 +132,6 @@ function locationTooltipsCorrect(input) {
     });
 }
 
-/***********************************
- * Load by ajax into index.html.
- ***********************************/
-
-$("#enterprises-form-ajax").load("../html/EnterprisesForm.html", function() {
-    $("#alert-form-enterprises").on("submit", function(e) {
-            e.preventDefault();
-
-            console.log($(this).serialize());
-            sendNewCompany();
-
-        })
-        //Listener Button, form enterprises.
-    $("#btn-enterprises").on("click", function() {
-        verifyFormEnterprises();
-    });
-});
-
-function createRequestBody(company) {
-    console.log('company :', company);
-    let formData = new FormData();
-    var fileField = document.querySelector("input[type='file']");
-    // Sent as a string
-    formData.append('name', company.name);
-    // Sent as a string
-    formData.append('CIF', company.CIF);
-
-    // Sent as a string with email validation
-    formData.append('email', company.email);
-
-    // Sent as a string
-    //  formData.append('city', company.address.city);
-
-    // Sent as a string
-    formData.append('country', company.country);
-    formData.append('zipcode', company.zipcode);
-
-    // Sent as a string
-    formData.append('street', company.street);
-
-    // Sent as a string
-    formData.append('bio', company.bio);
-
-    // Sent as a string
-    formData.append('employees', company.employees);
-
-    // Sent as a string. Choose a value from below.
-    formData.append('phone', company.phone);
-    formData.append('logoURL', fileField.files[0]);
-
-    //  formData.append('logo', company.logoURL.files[0]);
-
-    // Pick the value from an input(type="date")
-    console.log('formData :', formData);
-    return formData;
-}
-
-
-
-
-function sendNewCompany() {
-
-    let company = new Company(
-        $("input[name=name]").val(),
-        $("input[name=CIF]").val(),
-        $("input[name=country]").val(),
-        $("input[name=city]").val(),
-        $("input[name=street]").val(),
-        $("input[name=phone]").val(),
-        $("input[name=email]").val(),
-        $("input[name=employees]").val(),
-        $("input[name=website]").val(),
-        $("input[name=bio]").val(),
-        $("input[name=logoURL]").val()
-    );
-    console.log(' $("input[name=name]").val() :', $("input[name=name]").val());
-    let formBody = createRequestBody(company);
-
-    fetch("https://cv-mobile-api.herokuapp.com/api/company", {
-            method: 'POST',
-            mode: "no-cors",
-            body: formBody,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-
-        })
-        .then(function(res) {
-            console.log('res :', res);
-            res.json()
-        })
-        .then(response => console.log(response));
-}
-
 function EnterprisesForm() {
 
     this.construct = function(container) {
@@ -175,3 +152,16 @@ function EnterprisesForm() {
 }
 
 let enterpriseForm = new EnterprisesForm();
+
+
+/***********************************
+ * Load by ajax into index.html.
+ ***********************************/
+
+// $("#enterprises-form-ajax").load("../html/EnterprisesForm.html", function() {
+//     //Listener Button, form enterprises.
+//     $("#btn-enterprises").on("click", function() {
+//         sendNewCompany();
+//         verifyFormEnterprises();
+//     });
+// });
